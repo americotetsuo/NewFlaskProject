fc8729723c4bb2bf5421d637e5410c1029047539
americotetsuo <americotetsuo@gmail.com>
2023-10-10 14:51:27 -0300

    UPDATE 10.10.2023

--- webapp/config.py
+++ webapp/config.py
@@ -3,7 +3,10 @@ import os
 
 class Config:
     SECRET_KEY = '5791628bb0b13ce0c676dfde280ba245'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    ### OLD LOCAL DATABASE 
+    ### SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    ### NEW POSTGRESQL DATABASE
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@10.1.30.198:5432/webapp'
     MAIL_SERVER = 'smtp.outlook.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
